0fbb62dab3b454446b56498b848dc4a77533679f
namicad <namicad@gmail.com>
2026-07-24 02:32:13 +0900

    ..

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
-=======
 mod action;
 mod environment;
 mod reward;
 mod state;
->>>>>>> 73a33c58d1093a8551ad25e28b386e0db53ed088
 mod battle;
 mod info;
 mod pokedex;
 mod techdex;
 
-<<<<<<< HEAD
-=======
+
 pub use action::{Action, ActionError};
 pub use environment::{Environment, Observation, StepOutcome};
 pub use reward::{FAINT_REWARD, HP_PROGRESS_REWARD, LOSS_REWARD, WIN_REWARD, calculate_reward};
@@ -19,7 +15,6 @@ pub use state::{
     BattleObservation, BattleState, OpponentObservation, PokemonState, StateError,
     TeamPreviewObservation, TeamState,
 };
->>>>>>> 73a33c58d1093a8551ad25e28b386e0db53ed088
 pub use pokedex::{
     build_pokemon_from_pokedex, build_pokemon_from_pokedex_with_item, find_pokemon, pokedex,
 };
