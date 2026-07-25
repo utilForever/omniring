722e4da68ae4e18b6fbe6dd97b0acc25fee70847
namicad <namicad@gmail.com>
2026-07-25 15:46:21 +0900

    sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,34 @@
 mod action;
-mod battle_logic;
+mod battle;
 mod environment;
-mod info;
-mod pokedex;
 mod reward;
 mod state;
-mod techdex;
 
 pub use action::{Action, ActionError};
+pub use battle::Battle;
+pub use environment::{Environment, Observation, StepOutcome};
+pub use reward::{FAINT_REWARD, HP_PROGRESS_REWARD, LOSS_REWARD, WIN_REWARD, calculate_reward};
+pub use state::{
+    BattleObservation, BattleState, OpponentObservation, PokemonState, StateError,
+    TeamPreviewObservation, TeamState,
+};
+
+
+mod battle_logic;
+mod info;
+mod pokedex;
+mod techdex;
+
 pub use battle_logic::{
     AttackOutcome, DAMAGE_RANDOM_PERCENT_TABLE, DAMAGE_RANDOM_RAW_MAX, DAMAGE_RANDOM_RAW_MIN,
     DamageModifiers, TurnOrder, damage_random_percent_from_raw, determine_turn_order,
     execute_attack, simulate_turn,
 };
+pub use info::{
+    BattleFormat, ChampionsRules, HeldItem, MegaStone, Nature, Pokemon, StatPoints,
+    type_effectiveness_against,
+};
 pub use pokedex::{
     build_pokemon_from_pokedex, build_pokemon_from_pokedex_with_item, find_pokemon, pokedex,
-};
-pub use reward::{FAINT_REWARD, HP_PROGRESS_REWARD, LOSS_REWARD, WIN_REWARD, calculate_reward};
-pub use state::{
-    BattleObservation, BattleState, OpponentObservation, PokemonState, StateError,
-    TeamPreviewObservation, TeamState,
 };
 pub use techdex::{find_move, techdex};
